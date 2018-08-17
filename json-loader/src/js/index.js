69aac2de0d00125b71a597713a6d31bdff06570a
Luis Silva <luis.roberto.silva.guillen@gmail.com>
2018-08-16 23:52:57 -0300

    Soporte Json, Fix nombres folders

---
+++ json-loader/src/js/index.js
@@ -0,0 +1,22 @@
+import '../css/estilos.css';
+import makeMessage from './make-message.js';
+import renderToDOM from './render-to-dom';
+import { firstMessage, delayedMessage } from './messages.js';
+import platziImg from '../images/platzi.png';
+import data from './teachers.json';
+console.log(data);
+
+data.teachers.forEach(teacher => {
+    const element = document.createElement('li');
+    element.textContent = teacher.name;
+    renderToDOM(element);
+});
+
+const img = document.createElement('img')
+img.setAttribute('src', platziImg);
+img.setAttribute('width', 50);
+img.setAttribute('height', 50);
+document.body.append(img);
+
+renderToDOM(makeMessage(firstMessage));
+delayedMessage();
