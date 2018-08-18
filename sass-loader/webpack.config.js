9caa6a09bec94e0c05663a663a8d2e0fdd7c43a3
Luis Silva <luis.roberto.silva.guillen@gmail.com>
2018-08-17 23:07:07 -0300

    Soporte Sass

---
+++ sass-loader/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+    publicPath: "./dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // use: [
+        //   { loader: "style-loader" }, // Agrega el css al DOM en un <style>
+        //   { loader: "css-loader" }, // interpreta los archivos css en js via import
+        // ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        // use: [
+        //   { loader: "style-loader" }, // Agrega el css al DOM en un <style>
+        //   { loader: "css-loader" }, // interpreta los archivos css en js via import
+        // ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|eot|svg|ttf|woff)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css")
+  ]
+}
