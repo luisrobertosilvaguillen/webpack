99bb41089d20c76e6a03f20cdc04e4bd0160da48
Luis Silva <luis.roberto.silva.guillen@gmail.com>
2018-08-18 19:29:02 -0300

    Soporte Less

---
+++ less-loader/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+    publicPath: "./dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", 
+          {
+            loader: 'stylus-loader',
+            options: {
+              use: [
+                require('nib'),
+                require('rupture')
+              ],
+              import: [
+                '~nib/lib/nib/index.styl',
+                '~rupture/rupture/index.styl'
+              ]
+            }
+          }]
+        })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", {
+            loader: "less-loader",
+            options: {
+              notIeCompat: true
+            }
+          }]
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|eot|svg|ttf|woff)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css")
+  ]
+}
