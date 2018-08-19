fbe5599ea7ff9bac33d9dfb1e18ceec649267975
Luis Silva <luis.roberto.silva.guillen@gmail.com>
2018-08-18 21:58:00 -0300

    Optimizando paquetes comunes

---
+++ dll/dist/js/bundle.js
@@ -0,0 +1,33 @@
+webpackJsonp([0],{
+
+/***/ 20:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(9);
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(11);
+
+var _teachers = __webpack_require__(17);
+
+var _teachers2 = _interopRequireDefault(_teachers);
+
+var _teachers3 = __webpack_require__(18);
+
+var _teachers4 = _interopRequireDefault(_teachers3);
+
+__webpack_require__(19);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _reactDom.render)(_react2.default.createElement(_teachers2.default, { data: _teachers4.default }), document.getElementById("container"));
+
+/***/ })
+
+},[20]);
